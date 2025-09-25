23f555eea77b758af3b345483e3e0c7a1c3f0105
QuayeAdelaideOtoobea <quayeadelaide18@gmail.com>
2025-09-25 11:18:17 +0100

    added shapes

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+shapes_list = {1:"cone", 2:"circle" , 3:"square", 4:"kite" }
+
+
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/whereami')
+def whereami():
+    return 'Ghana!'
+
+@app.route('/getallitems')
+def shapes():
+    return shapes_list
+
+
+
+
+    
 
 
 if __name__ == '__main__':
